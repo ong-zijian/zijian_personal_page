e2898b9940ef966dc6e7eec56bd5775e3cf3c4e5
ong-zijian <zijian.ong.2021@scis.smu.edu.sg>
2023-01-10 11:34:09 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import johannes from './image/johannes-plenio.jpg'
+import Navbar from './components/navbar';
+import './stylesheet/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar/>
+      <div className='container'>
+        <div>
+        <img src={johannes} alt="Landscape with a boat" className='johannes'></img>
+        </div>
+        <p>Credit Johaness Pieno, retrieved from: https://unsplash.com/photos/DKix6Un55mw</p>
+      </div>
+
     </div>
+    
   );
 }
 
---
+++ src/components/navbar.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "../stylesheet/navStyle.css"
+
+export default class Navbar extends React.Component{
+    state = {
+
+    };
+
+    render() {
+        return(
+            <nav className="navi">
+                <a href="/" className="home-name">
+                    Ong Zi Jian
+                </a>
+                <div className="navigation-menu">
+                    <ul>
+                        <li>
+                        <a href="/home">Home</a>
+                        </li>
+                        <li>
+                        <a href="https://www.linkedin.com/in/ongzijian/">LinkedIn</a>
+                        </li>
+                        <li>
+                        <a href="https://github.com/ong-zijian?tab=projects">GitHub</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </nav>
+        )
+    }
+}
+
