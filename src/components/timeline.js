cadeacd81934118c8a06be695c6ed8edad3299ae
ong-zijian <zijian.ong.2021@scis.smu.edu.sg>
2023-01-16 12:31:54 +0800

    Created Timeline

---
+++ src/components/timeline.js
@@ -0,0 +1,148 @@
+import React from "react";
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+
+export default function timeline(){
+        return (
+        <VerticalTimeline>
+        
+        {/* SMU Events */}
+        <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: 'rgb(33, 150, 243)', color: '#000' }}
+            contentArrowStyle={{ borderRight: '7px solid  rgb(33, 150, 243)' }}
+            date="2021 - present"
+            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            //icon={<WorkIcon />}
+        >
+            <div style={{color:"#fff"}}>
+            <h3 className="vertical-timeline-element-title">SMU</h3>
+            <h4 className="vertical-timeline-element-subtitle">BSc Information System</h4>
+            <ul>
+                <li>Vue.js project: Assisted in creating a simple backend using Firebase. <a href="https://github.com/BeforeClass/web-frontend" style={{color:"whitesmoke"}}>Project</a></li>
+                <li>Basic web skills: HTML, CSS, PHP, Bootstrap</li>
+                <li>Python, Java</li>
+            </ul>
+            </div>
+        </VerticalTimelineElement>
+
+        {/* PolicyPal Internship */}
+        <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="May 2022  - July 2022"
+            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            //icon={<WorkIcon />}
+        >
+            <h3 className="vertical-timeline-element-title">FinTech Content & Develoment Intern</h3>
+            <h4 className="vertical-timeline-element-subtitle">PolicyPal - ValueChampion</h4>
+            <p>My internship at PolicyPal and Valuechampion consist mainly of writing articles and managing their database.</p>
+            <ul>
+                <li>Articles include topics like personal finance, insurance, and credit cards.</li>
+                <li>Researched Web 3.0 and help ideate FinTech course content.</li>
+            </ul>
+        </VerticalTimelineElement>
+
+        {/* National Service (NS) */}
+        <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="Oct 2019 - Jul 2021"
+            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            //icon={<WorkIcon />}
+        >
+            <h3 className="vertical-timeline-element-title">National Service</h3>
+            <h4 className="vertical-timeline-element-subtitle">3SG Signal</h4>
+            <p>CCNA basic certification. <br/>Learnt about setting LAN network and manage router & switch setting.</p>
+        </VerticalTimelineElement>
+
+        {/* EY Internship */}
+        <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="Sep 2018 - Feb 2019"
+            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
+            //icon={<WorkIcon />}
+        >
+            <h3 className="vertical-timeline-element-title">Business Analyst Intern</h3>
+            <h4 className="vertical-timeline-element-subtitle">EY</h4>
+            <p>My job scope includes market research, business analysis regarding FinTech/FIs, creating proposals, decks and marketing collateral.</p>
+            <ul>
+                <li>MATCH 2018:
+                    <ul>
+                        <li>Assisted in the curation of over 17,000 matches between the 380 participating investors and 840 enterprises. </li>
+                        <li>The event saw US$6.2 Billion of investment committed in ASEAN enterprises.</li>
+                        <li>Handled the matching algorithm, content creation and email management of the event.</li>
+                    </ul>  
+                </li>
+                <li>Deal Day 2018:
+                    <ul>
+                        <li>Part of the planning and execution team behind Deal Day 2018 on 5 October. </li>
+                        <li>Curated more than 400 meetings which received positive responses from the participants.</li>
+                        <li>Managed the meetings and schedule</li>
+                    </ul>
+                </li>
+                <li>Singapore FinTech Festival (SFF):
+                    <ul>
+                        <li>Actively engaged guest to the EY booth</li>
+                        <li>Promoting the department's services, EY FinTech Hub and thought leaderships.</li>
+                        <li>Helped in marketing the company and department.</li>
+                    </ul>
+                </li>
+                <li>EY FinTech-The New Black at EY Wavespace:
+                    <ul>
+                        <li>Part of the planning and execution team behind the event.</li>
+                        <li>Active engagement with the guest.</li>
+                    </ul>
+                </li>
+            </ul>
+        </VerticalTimelineElement>
+
+        {/* Polytechnic */}
+        <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            date="April 2016 - April 2019"
+            iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+            //icon={<SchoolIcon />}
+        >
+            <h3 className="vertical-timeline-element-title">Ngee Ann Polytechnic</h3>
+            <h4 className="vertical-timeline-element-subtitle">Diploma in Business Information Technology</h4>
+            <p>Mix course with elements of business, accounting and market with technology. Final year project includes a fullstack asp.net project</p>
+            <p>Hi Club</p>
+            <ul>
+                <li>Main Committee 17/18: Events Coordinator</li>
+                <li>Plan and organise 4 song signing event</li>
+                <li>Plan and organise 2 outreach events to interact with the Deaf community</li>
+            </ul>
+        
+        </VerticalTimelineElement>
+        {/* <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            date="November 2012"
+            iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+            //icon={<SchoolIcon />}
+        >
+            <h3 className="vertical-timeline-element-title">Agile Development Scrum Master</h3>
+            <h4 className="vertical-timeline-element-subtitle">Certification</h4>
+            <p>
+            Creative Direction, User Experience, Visual Design
+            </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+            className="vertical-timeline-element--education"
+            date="2002 - 2006"
+            iconStyle={{ background: 'rgb(233, 30, 99)', color: '#fff' }}
+            //icon={<SchoolIcon />}
+        >
+            <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
+            <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
+            <p>
+            Creative Direction, Visual Design
+            </p>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement
+            iconStyle={{ background: 'rgb(16, 204, 82)', color: '#fff' }}
+            //icon={<StarIcon />}
+        /> */}
+        </VerticalTimeline>
+    )
+}
+
+//reference: https://stephane-monnot.github.io/react-vertical-timeline/#/
