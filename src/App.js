4b8d1fac4d3a11e2c8a0304186a661ccca0484af
ong-zijian <zijian.ong.2021@scis.smu.edu.sg>
2023-01-23 21:53:07 +0800

    added node

--- src/App.js
+++ src/App.js
@@ -3,12 +3,31 @@ import johannes from './image/johannes-plenio.jpg'
 import Navbar from './components/navbar';
 import './stylesheet/App.css';
 import VerticalTimeline from './components/timeline.js'
+//import React, {useEffect, useState} from 'react';
 
 function App() {
+
+  // const [backendData, setBackendData] = useState([{}])
+  // useEffect(() =>{
+  //   fetch("/api").then(
+  //     response => response.json()
+  //   ).then(
+  //     data =>{
+  //       setBackendData(data)
+  //     }
+  //   )
+  // }, [])
+  
   return (
     <div className='mainDiv'>
       <Navbar/>
       <div className='container' class="mt-4">
+        {(typeof backendData.users === 'undefined') ? 
+        ( <p>Loading...</p>): (
+          backendData.users.map((user, i) => (
+            <p key={i}>{user}</p>
+          ))
+        )}
         <img src={johannes} alt="Credit Johaness Pieno, retrieved from: https://unsplash.com/photos/DKix6Un55mw" className='johannes' ></img>
         <p><i>Credit: Image by Johaness Pieno, retrieved from <a href="https://unsplash.com/photos/DKix6Un55mw">Unsplash</a></i></p>
       </div>
